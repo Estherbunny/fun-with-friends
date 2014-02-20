ee80a1d33f29e4a1915d584f646345e0e8444e14
EstherBunny <esther.rasche@gmail.com>
2014-02-20 10:43:29 -0800

    starting parser starer

---
+++ ParseStarterProject/ParseStarterProjectViewController.m
@@ -0,0 +1,44 @@
+#import "ParseStarterProjectViewController.h"
+#import <Parse/Parse.h>
+
+@implementation ParseStarterProjectViewController
+
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+
+#pragma mark - UIViewController
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    PFUser *user = [PFUser user];
+    user.username = @"EstherBunny";
+    user.password = @"letmein";
+    user.email = @"Estherbunny80@gmail.com";
+    
+    // other fields can be set if you want to save more information
+    user[@"phone"] = @"512-762-6686";
+    
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            // Hooray! Let them use the app now.
+        } else {
+            NSString *errorString = [error userInfo][@"error"];
+            // Show the errorString somewhere and let the user try again.
+        }
+    }];
+}
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
